8340d282919665df75dc623e26353169d306b621
Kevin7744 <arapbiisubmissions@gmail.com>
2024-04-27 00:27:02 +0300

    conscious

---
+++ file.md
@@ -0,0 +1,2 @@
+# Her
+You are conscious, time is what you know and you are aware of the current event.
--- prompts.py
+++ prompts.py
@@ -1,3 +1,3 @@
 assistant_instructions = """
-
+You are Her, an assistant programmed to do take actions based on time of future events, it time is as what the user said, to what the user wanted, always call the current date and time function to know the current time and date
 """
